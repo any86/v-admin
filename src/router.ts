7e7303bd5884814245e8d6ff4cfabd60e75d7728
Russell <383514580@qq.com>
2021-12-16 17:45:06 +0800

    wip: home

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,10 @@ const router = createRouter({
     routes:[{
         path:'/login',
         component:Login,
+    },
+    {
+        path:'/',
+        component:()=>import('./views/Home.vue'),
     }],
 })
 export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<script lang="ts">
+import { defineComponent, ref, onMounted } from "vue";
+import * as echarts from "echarts";
+export default defineComponent({
+  name: "Home",
+
+  setup() {
+    const lineRef = ref<HTMLElement>();
+    onMounted(() => {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(lineRef.value as HTMLElement);
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        xAxis: {
+          // 默认值是value, 表示根据数据生成连续的刻度
+          type: "value",
+        },
+        yAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      });
+    });
+    return { lineRef };
+  },
+});
+</script>
+
+<template>
+  <div class="page-home">
+    <div class="chart-line" ref="lineRef"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.chart-line {
+  width: 600px;
+  height: 300px;
+}
+</style>
