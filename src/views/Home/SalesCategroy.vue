2245a0c3f0ccd3fadb57c44ece7bf69eb51d2319
Russell <383514580@qq.com>
2022-01-11 17:39:00 +0800

    切换饼图数据

--- src/views/Home/SalesCategroy.vue
+++ src/views/Home/SalesCategroy.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent, ref, onMounted, watch } from "vue";
 import * as echarts from "echarts";
 import { http } from "@/http";
 export default defineComponent({
@@ -7,6 +7,9 @@ export default defineComponent({
 
   setup() {
     const salesCategoryChartRef = ref<HTMLElement>();
+    const type = ref<
+      "salesTypeData" | "salesTypeDataOnline" | "salesTypeDataOffline"
+    >("salesTypeData");
 
     onMounted(async () => {
       // 请求接口
@@ -19,33 +22,43 @@ export default defineComponent({
       // 基于准备好的dom，初始化echarts实例
       const myChart = echarts.init(salesCategoryChartRef.value as HTMLElement);
 
-      // 处理数据
-      const data = response.data.salesTypeData.map(({ x, y }) => {
-        return { name: x, value: y };
-      });
-
       // 绘制图表
-      myChart.setOption({
-        tooltip: {},
+      watch(
+        type,
+        (type) => {
+          // 切换不同类型的数据
+          const data = response.data[type].map(({ x, y }) => {
+            return { name: x, value: y };
+          });
 
-        series: [
-          {
-            name: "销售额",
-            type: "pie",
-            // 圆环的内径和外径比例
-            radius: ["40%", "70%"],
-            data,
-          },
-        ],
-      });
+          myChart.setOption({
+            tooltip: {},
+
+            series: [
+              {
+                type: "pie",
+                // 圆环的内径和外径比例
+                radius: ["40%", "70%"],
+                data,
+              },
+            ],
+          });
+        },
+        { immediate: true }
+      );
     });
-    return { salesCategoryChartRef };
+
+    return { salesCategoryChartRef, type };
   },
 });
 </script>
 
 <template>
-
+  <a-radio-group v-model:value="type">
+    <a-radio-button value="salesTypeData">全部</a-radio-button>
+    <a-radio-button value="salesTypeDataOnline">线上</a-radio-button>
+    <a-radio-button value="salesTypeDataOffline">线下</a-radio-button>
+  </a-radio-group>
   <div class="chart-sales-category" ref="salesCategoryChartRef"></div>
 </template>
 
