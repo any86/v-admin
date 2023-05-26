ee8af58c9269163772bab9a5f3ed41855f988c15
russell <383514580@qq.com>
2023-05-27 00:21:35 +0800

    wip

--- src/views/Text.vue
+++ src/views/Text.vue
@@ -1,19 +1,49 @@
 <script setup lang="ts">
-import { h, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import AScroll from 'any-scroll'
+import Core from '@any-scroll/core'
+import wheel from '@any-scroll/wheel'
 
-
+const count = ref(10);
+let at;
 onMounted(() => {
   const el = document.getElementById('abc')
   console.log(el);
   if (el) {
-    const at = new AScroll(el)
+    at = new AScroll(el, { allow: [true, false] })
+    // at.use(wheel, {})
   }
 })
+
+
+function add() {
+  count.value++;
+  at.update()
+}
 </script>
 
 <template>
-  <div id="abc">
-    <div>123123</div>
-  </div>
+  <article>
+    <div id="abc">
+      <ul>
+        <li v-for="n in count">第{{ n }}条</li>
+      </ul>
+    </div>
+    <button @click="add">增加</button>
+  </article>
 </template>
+
+<style scoped lang="scss">
+#abc {
+  border: 1px solid #000;
+}
+
+ul {
+  display: flex;
+
+  li {
+    list-style: none;
+    min-width: 200px;
+  }
+}
+</style>
